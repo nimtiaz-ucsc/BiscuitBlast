4d0aa8679fd99b908f54ba4babc23ad1df2ac73d
nimtiaz <nimtiaz@ucsc.edu>
2022-04-16 19:40:08 -0700

    more visual changes

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 /***********************
 Name: Nile Imtiaz
 Project name: Biscuit Beast's Biscuit Blast
-Start date: 4/11/2022
+Start date: 4/12/2022
 End date: 4/../2022
 ************************
 Points breakdown:
 - Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
 - Implement a simultaneous two-player mode (30)
 - Allow the player to control the Rocket after it's fired (5)
-- ... (5)
+- Implement the speed increase that happens after 30 seconds in the original game (5)
 ***********************/
 
 let config = {
